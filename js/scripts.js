cfb3fec28ec548214dab6f922ecef2128e7df390
Patrick Swaney <prswaney@gmail.com>
2020-07-16 15:51:58 -0700

    make a flash card site for viewing the definitions of JavaScript and jQuery.

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $(".1").click(function() {
+    $("#JavaScript-showing").toggle();
+    $("#JavaScript-hidden").toggle();
+  });
+  $(".2").click(function () {
+    $("#Operators-showing").toggle();
+    $("#Operators-hidden").toggle();
+  });
+  $(".3").click(function () {
+    $("#Variables-showing").toggle();
+    $("#Variables-hidden").toggle();
+  });
+  $(".4").click(function () {
+    $("#Variable-naming-conventions-showing").toggle();
+    $("#Variable-naming-conventions-hidden").toggle();
+  });
+  $(".5").click(function () {
+    $("#Functions-showing").toggle();
+    $("#Functions-hidden").toggle();
+  });
+  $(".6").click(function () {
+    $("#Methods-showing").toggle();
+    $("#Methods-hidden").toggle();
+  });
+  $(".7").click(function () {
+    $("#Arguments-showing").toggle();
+    $("#Arguments-hidden").toggle();
+  });
+  $(".8").click(function () {
+    $("#Parameters-showing").toggle();
+    $("#Parameters-hidden").toggle();
+  });
+  $(".9").click(function () {
+    $("#Return-value-showing").toggle();
+    $("#Return-value-hidden").toggle();
+  });
+  $(".10").click(function () {
+    $("#Chaining-methods-showing").toggle();
+    $("#Chaining-methods-hidden").toggle();
+  });
+  $(".11").click(function () {
+    $("#Strings-showing").toggle();
+    $("#Strings-hidden").toggle();
+  });
+  $(".12").click(function () {
+    $("#Booleans-showing").toggle();
+    $("#Booleans-hidden").toggle();
+  });
+  $(".13").click(function () {
+    $("#Undefined-showing").toggle();
+    $("#Undefined-hidden").toggle();
+  });
+  $(".14").click(function () {
+    $("#NaN-showing").toggle();
+    $("#NaN-hidden").toggle();
+  });
+  $(".15").click(function () {
+    $("#Null-showing").toggle();
+    $("#Null-hidden").toggle();
+  });
+  $(".16").click(function () {
+    $("#Alert-showing").toggle();
+    $("#Alert-hidden").toggle();
+  });
+  $(".17").click(function () {
+    $("#Comments-showing").toggle();
+    $("#Comments-hidden").toggle();
+  });
+  $(".18").click(function () {
+    $("#jQuery-showing").toggle();
+    $("#jQuery-hidden").toggle();
+  });
+  $(".19").click(function () {
+    $("#Attributes-showing").toggle();
+    $("#Attributes-hidden").toggle();
+  });
+});
